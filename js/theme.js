bf0c0951d50275d6436c573c564df4ee32277475
sangwookcheon <sangwookchn@gmail.com>
2019-10-18 11:04:28 +0700

    First Addition to the page. Change Style

--- js/theme.js
+++ js/theme.js
@@ -2,23 +2,23 @@ document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
 function theme() {
      if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-         
+
             sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
-            
-         
+            sessionStorage.setItem('cc', '#f5f5f5');
+
+
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
         sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
         sessionStorage.setItem('cc', '#777');
-        
+
     }
     else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-        
+
         sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
         sessionStorage.setItem('cc', '#333');
-        
-  
+
+
     }
 
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
@@ -26,4 +26,12 @@ document.body.style.color = sessionStorage.getItem('cc');
 
 }
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
